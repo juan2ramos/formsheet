d1a54ab054a886fcee3d18f2f23c710bb46c2b13
Michael Camacho <michaelcamacho@mi-martinez.com>
2018-06-18 15:09:20 -0500

    estilo form principal y home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,50 @@
 @section('content')
 <section class="container">
   <h2>Cotización Rápida</h2>
-  <p>Selecciona el tipo de servicio que deseas cotizar.</p>
-  <a href="{{route('business')}}">
-  <div class="row align-center justify-evenly botones-home">
-    <div class="col-6 icon-service">
-        <img src="{{asset('images/iconoRutaEmpresarial.png')}}" alt="">
-        <hr class="line">
+  <p class="is-text-center">Selecciona el tipo de servicio que deseas cotizar.</p>
+  <a class="botones-home is-full-width m-t-20" href="{{route('business')}}">
+    <div class="row align-center justify-around">
+      <div class="col-16 col-m-4 col-l-4 icon-service">
+          <img src="{{asset('images/iconoRutaEmpresarial.png')}}" alt="">
+      </div>
+      <div class="col-16 col-m-10 col-l-10">
+          <h3>Ruta Empresarial</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim, massa sed efficitur mattis, leo orci luctus diam, sed auctor </p>
+      </div>
     </div>
-    <div class="col-10">
-        <h3>Ruta Empresarial</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim, massa sed efficitur mattis, leo orci luctus diam, sed auctor </p>
+  </a>
+  <a class="botones-home is-full-width m-t-28" href="{{route('calculate')}}">
+    <div class="row align-center justify-around">
+      <div class="col-16 col-m-4 col-l-4 icon-service">
+          <img src="{{asset('images/iconoViajeFuera.png')}}" alt="">
+      </div>
+      <div class="col-16 col-m-10 col-l-10">
+          <h3>Viaje fuera de la ciudad</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim, massa sed efficitur mattis, leo orci luctus diam, sed auctor </p>
+      </div>
     </div>
-
-  </div>
+  </a>
+  <a class="botones-home is-full-width m-t-28" href="">
+    <div class="row align-center justify-around">
+      <div class="col-16 col-m-4 col-l-4 icon-service">
+          <img src="{{asset('images/iconTraslado.png')}}" alt="">
+      </div>
+      <div class="col-16 col-m-10 col-l-10">
+          <h3>Traslado dentro de la ciudad</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim, massa sed efficitur mattis, leo orci luctus diam, sed auctor </p>
+      </div>
+    </div>
+  </a>
+  <a class="botones-home is-full-width m-t-28" href="">
+    <div class="row align-center justify-around">
+      <div class="col-16 col-m-4 col-l-4 icon-service">
+          <img src="{{asset('images/iconoPuertaPuerta.png')}}" alt="">
+      </div>
+      <div class="col-16 col-m-10 col-l-10">
+          <h3>Transporte puerta a puerta</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim, massa sed efficitur mattis, leo orci luctus diam, sed auctor </p>
+      </div>
+    </div>
   </a>
 </section>
 
