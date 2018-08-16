c8cb48cc60c150b451891fc259cc39eccbd9bf29
juan2ramos <juan2ramos@gmail.com>
2018-08-16 17:03:01 -0500

    cities

--- resources/views/business.blade.php
+++ resources/views/business.blade.php
@@ -21,19 +21,19 @@
                     <p><span>1</span> Selecciona tu ciudad de origen </p>
                 </div>
                 <div class="col-16 col-m-4 col-l-4 m-t-8">
-                    <input id="cityBogota" class="rectangle" type="radio" checked name="origin" value="Ruta Empresarial Bogota">
+                    <input id="cityBogota" class="rectangle" type="radio" checked name="origin" value="Ruta-Empresarial-Bogota">
                     <label for="cityBogota">Bogotá</label>
                 </div>
                 <div class="col-16 col-m-4 col-l-4 m-t-8">
-                    <input id="cityVill" class="rectangle" type="radio" name="origin" value="Ruta Empresarial Villavicencio">
+                    <input id="cityVill" class="rectangle" type="radio" name="origin" value="Ruta-Empresarial-Medellin">
                     <label for="cityVill">Villavicencio</label>
                 </div>
                 <div class="col-16 col-m-4 col-l-4 m-t-8">
-                    <input id="cityCali" class="rectangle" type="radio" name="origin" value="Ruta Empresarial Cali">
+                    <input id="cityCali" class="rectangle" type="radio" name="origin" value="Ruta-Empresarial-Villavicencio">
                     <label for="cityCali">Calí</label>
                 </div>
                 <div class="col-16 col-m-4 col-l-4 m-t-8">
-                    <input id="cityMede" class="rectangle" type="radio" name="origin" value="Ruta Empresarial Medellin">
+                    <input id="cityMede" class="rectangle" type="radio" name="origin" value="Ruta-Empresarial-Cali">
                     <label for="cityMede">Medellín</label>
                 </div>
 
