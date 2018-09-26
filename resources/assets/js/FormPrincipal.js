bf0e6fb1b9206cd55b51173b263c1bbdad3fd1c2
juan2ramos <juan2ramos@gmail.com>
2018-09-26 11:04:43 -0500

    ok puerta a puerta

--- resources/assets/js/FormPrincipal.js
+++ resources/assets/js/FormPrincipal.js
@@ -11,7 +11,7 @@ const site = document.getElementById('body').dataset.site,
     inputAuto = document.querySelector('#destiny'),
     loadWrap = document.getElementById('loadWrap'),
     submitPrincipal = document.querySelector('#submitPrincipal'),
-    citiesData = JSON.parse(inputAuto.dataset.cities),
+
     scrollCoords = {
         y: window.pageYOffset
     };
@@ -82,13 +82,15 @@ export default class Principal {
                 'Villavicencio': 1,
                 'Medellín': '2',
                 'Calí': 3,
-            };
+            },
+            citiesData = JSON.parse(inputAuto.dataset.cities);
         let cityValue = 'BOGOTA';
         city.forEach(function (el) {
             el.addEventListener('change', function () {
                 inputAuto.value = "";
                 inputAuto.disabled = false
                 cityValue = this.value;
+
                 new autoComplete({
                     selector: '#destiny', minChars: 2,
                     source: function (term, suggest) {
